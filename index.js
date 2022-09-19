751b7446f3af65143d49939819c6f1a6d8c5d02b
Moin <moinuahmed2011@gmail.com>
2022-09-19 18:49:56 +0600

    loop

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let hour = 15;
+for (let i =0; i <10; i++){
+if (i % 2 !== 0) console.log(i);
+}
 
-if (hour >= 6 && hour <12)
-    console.log('Good mornig');
-else if(hour >=12 && hour < 18)
-    console.log('Good afternoon');
-else
-    console.log('Good evening');
-
+let i = 0;
+while ( i <= 5){
+    if (i % 2 !== 0) console.log(i);
+    i++;
+}
