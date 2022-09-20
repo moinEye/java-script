3471a311bd5fdefe103d0f80fd180d84fff8e764
Moin <moinuahmed2011@gmail.com>
2022-09-20 13:19:51 +0600

    fix:function

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-for (let i =0; i <10; i++){
-if (i % 2 !== 0) console.log(i);
-}
+const output = fizzBuzz(15);
+console.log(output);
 
-let i = 0;
-while ( i <= 5){
-    if (i % 2 !== 0) console.log(i);
-    i++;
+function fizzBuzz(input){
+    if (typeof input !== 'number')
+        return 'Not a number';
+
+    if ((input % 3 === 0) && (input % 5 === 0))
+        return 'FizzBuzz';
+
+
+
+    if(input % 3 === 0)
+        return 'Fizz';
+
+    if (input % 5 === 0)
+        return 'Buzz';
+
+    
+    return input;
+
 }
