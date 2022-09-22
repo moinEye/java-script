4cf27e99fb644c4add3f7df5cd2f374459bce548
Moin <moinuahmed2011@gmail.com>
2022-09-22 17:44:32 +0600

    add:report

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-console.log(sum(10));
+const array = [80, 80, 50]
 
-function sum (limit){
+console.log(calculateGrade(array));
+
+function calculateGrade(marks){
+
+    const average = calculateAverage(marks);
+    if (average < 60) return 'F';
+    if (average < 70) return 'D';
+    if (average < 80) return 'C';
+    if (average < 90) return 'B';
+    return 'A';
+}
+
+function calculateAverage(array) {
+
     let sum = 0;
-
-    for (let i=0; i<= limit; i++)
-        if (i % 3 === 0 || i % 5 === 0)
-            sum += i;
-
-    return sum;
+    for (let value of array)
+        sum += value;
+    return sum / array.length;
 
 }
+
+
 
