694518c61df3c706d12bd15398a8acd1357521d7
moinuahmed <moinuahmed2011@gmail.com>
2022-09-28 12:16:11 +0600

    object:basic

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-showPrimes(20);
-
-function showPrimes(limit){
-    for (let number =2; number <= limit; number++){
-        let isPrime = true;
-        for (let factor = 2; factor < number; factor++){
-            if (number % factor === 0){
-
-                isPrime = false;
-            }
-        }
-        if (isPrime) console.log(number);
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function(){
+        console.log('draw');
     }
-}
+};
