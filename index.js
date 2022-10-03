c1f3408a66e8346d8b238bcd1207b895efb91518
moinuahmed <moinuahmed2011@gmail.com>
2022-10-03 16:24:13 +0600

    array

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    isVisible: true,
-    draw: function(){
-        console.log('draw');
-    }
-};
+const numbers = [3,4];
+
+numbers.push(5,6);
+
+numbers.unshift(1,2);
+
+numbers.splice(2, 0, 'a', 'b' );
+
+console.log(numbers);
