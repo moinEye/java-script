6856864abd653c700577cd05c29c5b05ed5c25dd
moinuahmed <moinuahmed2011@gmail.com>
2022-10-04 13:04:29 +0600

    fix:find array

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const numbers = [1,2,3,1,4];
+const courses = [
 
-console.log(numbers.indexOf(1, 2));
-console.log(numbers.lastIndexOf(1));
+    {id: 1, name: 'a' },
+    {id: 2, name: 'b' },
 
-console.log(numbers.indexOf(1) !== -1);
-console.log(numbers.includes(1));
+];
+const course = courses.find(function(course) {
+    return course.name === 'nos';
+});
+
+console.log(course);
