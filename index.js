427c085f2009be1ff053d0a7412b835d52cefff7
moinuahmed <moinuahmed2011@gmail.com>
2022-10-10 18:22:32 +0600

    array: mapping

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ const numbers = [1, -1, 2, 3];
 
 const filtered = numbers.filter(n => n >= 0);
 
+const items = filtered.map(n => {
+    const obj = { value: n};
+    return obj;
+});
 
-console.log(filtered);
+
+console.log(items);
