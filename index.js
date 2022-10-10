6d848d82d1b7c67f87911c2855434ec8cf22cddb
moinuahmed <moinuahmed2011@gmail.com>
2022-10-10 12:05:51 +0600

    array:sort

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const numbers = [2, 3, 1];
-numbers.sort();
-console.log(numbers);
+const courses = [
+    {id: 1, name: 'Node.js'},
+    {id: 2, name: 'zavaScript'},
+];
 
-numbers.reverse();
-console.log(numbers);
+courses.sort(function(a,b){
+
+    const nameA = a.name.toLowerCase();
+    const nameB = b.name.toLowerCase();
+
+    if (nameA < nameB) return -1;
+    if (nameA > nameB) return 1;
+    return 0;
+
+});
+
+
+
+console.log(courses);
