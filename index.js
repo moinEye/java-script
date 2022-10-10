d6c25fd09ab372d66fbc246103f3ca93e3a6aab9
moinuahmed <moinuahmed2011@gmail.com>
2022-10-10 12:33:48 +0600

    array: filtering

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const numbers = [1, -1, 2, 3];
 
-const atLeastOnePositive = numbers.some(function(value){
-    return value >= 0;
-});
+const filtered = numbers.filter(n => n >= 0);
 
-console.log(atLeastOnePositive);
+
+console.log(filtered);
